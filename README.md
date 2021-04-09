d6b4803ccbb28d289abd85a0f41f085735f07a64
Alex Miller <alex.miller@anchore.com>
2021-04-08 22:16:24 -0400

    python Click CLI

--- README.md
+++ README.md
@@ -2,101 +2,176 @@
 
 This utility provides a convenient way to manage an Anchore policy bundle as individual components. 
 
-Running this within an anchore/engine-cli container requires adding packages `jq` & `diffutils`. A sample Dockerfile with these modifications is available in `tools/modular-bundle/Dockerfile`
+## Usage
+
+```bash
+./anchore-bundle --help
+```
 
 ## Modular Policy Demo
 
-1. Clone this repo and cd into it
-2. Download the [Anchore CIS bundle](https://github.com/anchore/hub/blob/master/sources/bundles/anchore_cis_1.13.0_base.json) into this dir
-3. Extract the bundle into components: `./extract.sh anchore_cis_1.13.0_base.json`
-    - Review the extracted components: `tree bundle`
-4. Modify the example to check for your own base image:
-    ```bash
-    sed -i .bak \
-      's/example_trusted_base1,example_trusted_base2/debian:stable-slim,debian:stretch-slim/' \
-      bundle/policies/cb417967-266b-4453-bfb6-9acf67b0bee5.json
-    ```
-5. Generate a new bundle: `./generate.sh`
-    - Review the generated files, and compare the generated bundle with the original:
-        ```bash
-        cat bundle_id
-        jq . bundle.json | less
-        diff <(jq --sort-keys . bundle.json) <(jq --sort-keys . anchore_cis_1.13.0_base.json)
-        ```
-6. Push bundle to Anchore and set as active:
-    ```bash
-    anchore-cli policy add bundle.json && anchore-cli policy activate $(cat bundle_id)
-    ```
-7. Scan images using your new policy bundle!
+This demo requires a working Anchore deployment. Refer to the docker-compose [Quickstart](https://docs.anchore.com/current/docs/quickstart/) if you need to provision one.
 
-Repeat steps 4-7 with your own modifications on an ongoing basis. Steps 5 & 6 can be automated with a CI tool to always keep your active policy up to date with a branch of this repo.
+### Demo Setup
+All files used in this demo, including the [Anchore CIS bundle](https://github.com/anchore/hub/blob/master/sources/bundles/anchore_cis_1.13.0_base.json), are contained in the `sample_input` dir. These files are copied into `/anchore-cli/` during the following container build:
+```bash
+docker build -t anchore-bundle:demo .
+
+# Modify variables as needed to work in your environment
+docker run -it --rm --network=host \
+  -e ANCHORE_CLI_USER=admin \
+  -e ANCHORE_CLI_PASS=foobar \
+  -e ANCHORE_CLI_URL=http://localhost:8228/v1 \
+  anchore-bundle:demo -- bash
+
+# Add the original CIS bundle to Anchore and activate it:
+anchore-cli policy add anchore_cis_1.13.0_base.json
+anchore-cli policy activate anchore_cis_1.13.0_base
+
+# Define the image to use for this demo
+export IMG=docker.io/thinkmassive/hello-world:alpine-3.13
+
+# Add the demo image to Anchore:
+anchore-cli image add $IMG
+
+# Analyze the demo image w/CIS bundle:
+#  (the process should succeed, but 'Final action: stop' is expected)
+anchore-cli evaluate check $IMG --detail | tee eval-1.out
+
+# Enable tab-completion for anchore-bundle (optional)
+eval "$(_ANCHORE_BUNDLE_COMPLETE=source_bash anchore-bundle)"
+```
+
+All of the following steps should be run in the container created in Demo Setup.
+
+### Policy Management Demo
+
+1. Extract original bundle into components and review the output.
+
+```bash
+# Extract the CIS bundle into components:
+anchore-bundle extract anchore_cis_1.13.0_base.json
+
+# Review the extracted components:
+ls -R bundle
+
+# Review the bundle template, notice how each component item only has an id field:
+python -m json.tool bundle/template.json
+```
+
+2. Modify the bundle and review changes.
+
+```bash
+# Backup the policy file before editing:
+cp bundle/policies/cb417967-266b-4453-bfb6-9acf67b0bee5.json{,.bak}
+
+# Change the bundle_id for easy comparison:
+sed -i 's/"id": "anchore_cis_1.13.0_base"/"id": "demo_1"/' bundle/template.json
+
+# Modify the example to always allow our base image:
+sed -i 's/example_trusted_base1,example_trusted_base2/alpine:3.13,scratch/' \
+  bundle/policies/cb417967-266b-4453-bfb6-9acf67b0bee5.json
+
+# Review the change:
+diff bundle/policies/cb417967-266b-4453-bfb6-9acf67b0bee5.json{,.bak}
+```
+
+3. Generate a new bundle with our modifications, and review the output.
+
+```bash
+# Generate a new bundle:
+anchore-bundle generate
+
+# Display the generated bundle_id
+cat bundle_id ; echo
+
+# Review the generated bundle, notice how component items are merged back into the template:
+python -m json.tool bundle.json | more
+
+# Compare the generated bundle with the original:
+diff <(python -m json.tool --sort-keys bundle.json) \
+  <(python -m json.tool --sort-keys anchore_cis_1.13.0_base.json)
+
+# Push the bundle to Anchore and set as active:
+anchore-cli policy add bundle.json && anchore-cli policy activate $(cat bundle_id)
+```
+
+4. Scan images using the modified policy bundle. The result should now be `Final action: warn` instead of `stop`, because the `Dockerfile directive 'FROM' check` is gone.
+
+```bash
+anchore-cli evaluate check $IMG --detail | tee eval-2.out
+
+diff eval-1.out eval-2.out
+```
+
+Repeat steps 2-4 with your own modifications on an ongoing basis. Step 3 can be automated with a CI tool to always keep your active policy up to date with a branch of this repo.
 
 ### Auto-whitelist Demo
 
-The `allow-stopped.py` script can be run as step 4 above. The following demo assumes a bundle was extracted according to the steps above, and it uses example policy evaluation output for the ubi8-minimal image from Iron Bank, found in the `sample_inputs` dir of this repo.
+The `anchore-bundle allow` subcommand can be run during step 2 above. The following demo assumes a bundle was extracted according to the steps above, and it uses example policy evaluation output for the ubi8-minimal image from Iron Bank, found in the `sample_inputs` dir of this repo.
 
 ```bash
-# make the script executable
-chmod +x allow-stopped.py
+# Generate new allowlist, based on eval output (compliance report, gates.csv, security.csv)
+./anchore-bundle allow \
+    -c sample_input/compliance_reports/ubi8-minimal_8.3.json \
+    -g sample_input/gates/ubi8-minimal_8.3.csv \
+    -s sample_input/security/ubi8-minimal_8.3.csv
 
-# review the input files
-tree sample_input/
+# Generate mapping to include new allowlist in bundle
+./anchore-bundle map demo-ubi8-minimal demo-ubi8-minimal --repo '*/ubi8-minimal'
 
-# generate a whitelist from the sample ubi8-minimal data
-./allow-stopped.py \
-  sample_input/compliance_reports/ubi8-minimal_8.3_2021-03-24T23_59_55.843Z.json \
-  sample_input/gates/ubi8-minimal_8.3.csv \
-  sample_input/security/ubi8-minimal_8.3.csv \
-  bundle/whitelists
-
-# review the output
-jq . bundle/whitelists/demo-ubi8-minimal.json | less
+# Generate bundle with new components added
+./anchore-bundle generate
 ```
 
 ---
 
-## Utilities
+## Runbook
 
-### extract.sh
-
-Extracts an existing bundle into components.
-
+### Use environment variables for configuration
 ```bash
-myBundle=anchore_cis_1.13.0_base.json
-componentDir=bundle
-
-./extract.sh $mybundle $componentDir
-
-tree $componentDir
-```
-### generate.sh
-
-Generates a new bundle from components.
-
-```bash
-componentDir=bundle
-
-./generate.sh $componentDir
-
-cat bundle_id
-less bundle.json
 ```
 
-#### generate.sh output files
+### Extract bundle into components
+
+```bash
+anchore-bundle extract $SOURCE
+```
+
+### Generate bundle from components
+
+```bash
+anchore-bundle generate
+```
+
   - `bundle.json` is the generated policy bundle
   - `bundle_id` contains the generated bundle id
 
-### allow-stopped.py
+### Allow all stop actions
 
-Generates a new whitelist for a specified repo:tag for all "stop" actions in a compliance report.
-
-Integrating the new whitelist into the bundle dir is still in progress.
+Generates a new allowlist for a specified repo:tag for all "stop" actions in a compliance report.
 
 ```bash
-report=Compliance_Report_2021-01-07T21_46_39.535Z.csv
-output_dir=new_whitelist
+GATES=gates.csv
+SECURITY=security.csv
+COMPLIANCE=compliance_report.json
 
-python allow-stopped.py  $report  $output_dir
-
-ls -l $output_dir
+anchore-bundle allow -g $GATES -s $SECURITY -c $COMPLIANCE
 ```
+
+### Map new allowlist 
+```bash
+IMG='docker.io/MyImage:*'
+ALLOWLIST=MyImageAllowlist
+MAPPING=MyImageMapping
+
+anchore-bundle map -p $IMG $ALLOWLIST $MAPPING
+```
+
+---
+
+## Continuous Integration
+
+You may choose to fork this repo and use it to store your bundle components, in which case a CI build job can be used to keep the active bundle up to date.
+
+The sample files and docs for this are still in progress.
---
+++ bundle.py
@@ -0,0 +1,343 @@
+import csv
+import hashlib
+import json
+import os
+
+# ----------
+# constants
+# ----------
+BUNDLE_COMPONENTS = [
+        'mappings',
+        'policies',
+        'whitelists',
+        'whitelisted_images',
+        'blacklisted_images'
+]
+
+GATES_CSV_FIELDS = {
+    'trigger_id': 2,
+    'gate': 3,
+    'gate_action': 6,
+    'policy_id': 7,
+    'whitelist_id': 9,
+    'justification': 12
+}
+
+SECURITY_CSV_FIELDS = {
+    'cve': 1,
+    'severity': 2,
+    'package': 5,
+    'justification': 17
+}
+
+# -----------------
+# shared functions
+# -----------------
+
+
+def read_json_file(json_file):
+    try:
+        with open(json_file, "r") as r_file:
+            return json.load(r_file)
+    except OSError as e:
+        print(f'error opening JSON file: {e}')
+
+
+def write_json_file(json_obj, json_file, formatted=True):
+    try:
+        with open(json_file, "w") as w_file:
+            if formatted:
+                w_file.write(
+                        json.dumps(json_obj, indent=2, separators=(',', ': ')))
+            else:
+                w_file.write(json.dumps(json_obj))
+            w_file.close()
+            print(f'wrote {json_file}')
+    except OSError as e:
+        print(f"error writing {json_file}: {e}")
+
+
+def write_text_file(txt, txt_file):
+    try:
+        with open(txt_file, 'w') as w_file:
+            w_file.write(txt)
+            w_file.close()
+            print(f'wrote {txt_file}')
+    except OSError as e:
+        print(f"error writing {txt_file}: {e}")
+
+
+def read_bundle_array(json_array, array_name, bundle_dir):
+    bundle_array = []
+    if len(json_array) == 0:
+        print(f'(no {array_name})')
+    for json_item in json_array:
+        json_file = os.path.join(
+                bundle_dir,
+                array_name,
+                json_item['id'] + '.json'
+        )
+        bundle_item_json = read_json_file(json_file)
+        bundle_array.append(bundle_item_json)
+        print(f'read {json_file}')
+    return bundle_array
+
+
+def dump_json_array(json_array, json_name, bundle_dir):
+    for json_item in json_array:
+        json_file = os.path.join(
+                bundle_dir,
+                json_name,
+                json_item['id'] + '.json'
+        )
+        write_json_file(json_item, json_file)
+
+
+def read_csv_file(csv_file, csv_fields, debug=False):
+    csv_list = []
+    try:
+        with open(csv_file, "r") as r_file:
+            csv_reader = csv.reader(r_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    if debug:
+                        print(f'Column names are {", ".join(row)}')
+                else:
+                    csv_row = {}
+                    for key, value in csv_fields.items():
+                        csv_row[key] = row[value]
+                    csv_list.append(csv_row)
+                line_count += 1
+            if debug:
+                print(f'Processed {line_count} CSV rows.')
+            return csv_list
+    except OSError as e:
+        print(f'error processing CSV file {csv_file}: {e}')
+
+
+def get_bundle_description(bundle_json):
+    if 'description' in bundle_json:
+        return bundle_json['description']
+    if 'comment' in bundle_json:
+        return bundle_json['comment']
+    return ''
+
+
+# ---------------------
+# subcommand: generate
+# ---------------------
+def generate_bundle(ctx):
+    bundle_dir = ctx.obj['bundle_dir']
+
+    template_file = bundle_dir + '/template.json'
+    print(f'Generating bundle from {bundle_dir}')
+    template_json = read_json_file(template_file)
+
+    bundle_id = template_json['id']
+    bundle_json = template_json
+    print(f'Bundle id: {bundle_id}')
+    for component in BUNDLE_COMPONENTS:
+        bundle_json[component] = read_bundle_array(
+                template_json[component], component, bundle_dir)
+
+    bundle_json_file = 'bundle.json'
+    bundle_id_file = 'bundle_id'
+    write_json_file(bundle_json, bundle_json_file)
+    write_text_file(bundle_id, bundle_id_file)
+
+
+# --------------------
+# subcommand: extract
+# --------------------
+def extract_bundle(ctx, input_file):
+    bundle_dir = ctx.obj['bundle_dir']
+    print(f'Extracting bundle {input_file.name} into dir {bundle_dir}')
+
+    # Read original bundle JSON file
+    bundle_json = read_json_file(input_file.name)
+
+    # Create bundle directory structure
+    try:
+        os.makedirs(bundle_dir, exist_ok=True)
+        for component in BUNDLE_COMPONENTS:
+            bundle_dir_path = os.path.join(bundle_dir, component)
+            os.makedirs(bundle_dir_path, exist_ok=True)
+    except OSError as e:
+        print(f'error creating bundle directory or its subdirectories: {e}')
+
+    # Populate template json
+    template_file = os.path.join(bundle_dir, 'template.json')
+    bundle_description = get_bundle_description(bundle_json)
+    template_json = {
+            'id': bundle_json['id'],
+            'name': bundle_json['name'],
+            'version': bundle_json['version'],
+            'description': bundle_description,
+            'mappings': [],
+            'policies': [],
+            'whitelists': [],
+            'whitelisted_images': [],
+            'blacklisted_images': [],
+    }
+    for component in BUNDLE_COMPONENTS:
+        for i in bundle_json[component]:
+            template_json[component].append({'id': i['id']})
+
+    # Write template file
+    write_json_file(template_json, template_file)
+
+    # Write components files
+    for component in BUNDLE_COMPONENTS:
+        dump_json_array(bundle_json[component], component, bundle_dir)
+
+    print('Bundle extraction complete')
+
+
+# ------------------
+# subcommand: allow
+# ------------------
+def allowlist_json_from_eval(ctx, compliance_file, gates_file, security_file):
+    bundle_dir = ctx.obj['bundle_dir']
+    debug = ctx.obj['debug']
+    if debug:
+        print('generating allowlist')
+        print(f'bundle_dir: {bundle_dir}')
+        print(f'compliance report: {compliance_file}')
+        print(f'gates report: {gates_file.name}')
+        print(f'security (CVEs) report: {security_file.name}')
+
+    compliance_json = read_json_file(compliance_file.name)
+
+    # Container image name will be used to determine allowlist filename
+    container_image = compliance_json['metadata']['repository']
+
+    # Read gates file (csv)
+    # image_id,repo_tag,trigger_id,gate,trigger,check_output,gate_action,policy_id,matched_rule_id,whitelist_id,whitelist_name,inherited,Justification
+    gates = read_csv_file(gates_file.name, GATES_CSV_FIELDS)
+
+    # Read security file (csv)
+    # tag,cve,severity,feed,feed_group,package,package_path,package_type,package_version,fix,url,inherited,description,nvd_cvss_v2_vector,nvd_cvss_v3_vector,vendor_cvss_v2_vector,vendor_cvss_v3_vector,Justification
+    cves = read_csv_file(security_file.name, SECURITY_CSV_FIELDS)
+
+    # Find an existing allowlist_id, otherwise return md5 hash of
+    #  trigger_id+container_image name
+    # TODO: determine if this is compatible with existing policy handling
+    def getAllowlistId(trigger_id):
+        default_allowlist_id = hashlib.md5(
+                str(trigger_id + container_image).encode()).hexdigest()
+        for gates_item in gates:
+            if gates_item['trigger_id'] == trigger_id:
+                if (gates_item['whitelist_id'] is None) or \
+                        (gates_item['whitelist_id'] == ''):
+                    return default_allowlist_id
+                else:
+                    return gates_item['whitelist_id']
+        return default_allowlist_id
+
+    # Find an existing justification, otherwise return "new"
+    def getJustification(trigger_id):
+        refer_to_cve = 'See Anchore CVE Results sheet'
+        justification = ''
+        for gates_item in gates:
+            if gates_item['trigger_id'] == trigger_id:
+                if gates_item['justification'] == refer_to_cve:
+                    trigger_split = gates_item['trigger_id'].split('+')
+                    trigger_cve = trigger_split[0]
+                    trigger_pkg = trigger_split[1]
+                    for cves_item in cves:
+                        if (cves_item['cve'] == trigger_cve) and \
+                                (cves_item['package'].startswith(trigger_pkg)):
+                            justification = cves_item['justification']
+                else:
+                    justification = gates_item['justification']
+
+        if justification:
+            return justification
+        else:
+            return 'new'
+
+    # Generate new allowlist from items in compliance report
+    allowlist = []
+    for item in compliance_json['policyEvaluation']:
+        if item['gateAction'] in ['stop', 'warn']:
+            allowlist_item = {
+                'id': getAllowlistId(item['triggerId']),
+                'trigger_id': item['triggerId'],
+                'gate': item['gate'],
+                'comment': getJustification(item['triggerId'])
+            }
+            allowlist.append(allowlist_item)
+
+    # allowlist filename based on container image name
+    allowlist_name = container_image.replace('/', '-')
+    allowlist_file = bundle_dir + '/whitelists/' + allowlist_name + '.json'
+    allowlist_file = os.path.join(
+            bundle_dir,
+            'whitelists',
+            allowlist_name + '.json'
+    )
+
+    # write new allowlist to file
+    if debug:
+        print(f'writing allowlist_file: {allowlist_file}')
+    allowlist_json = {
+        "comment": allowlist_name + " allowlist",
+        "id": allowlist_name + "Allowlist",
+        "items": allowlist,
+        "name": allowlist_name + " Allowlist",
+        "version": "1_0"
+    }
+    write_json_file(allowlist_json, allowlist_file, False)
+    if debug:
+        for item in allowlist:
+            print(item)
+
+
+# ----------------
+# subcommand: map
+# ----------------
+def map_allow(ctx, allowlist_id, mapping_id, mapping_position, registry_pattern, repo_pattern, tag_pattern):
+    debug = ctx.obj['debug']
+    bundle_dir = ctx.obj['bundle_dir']
+    template_file = bundle_dir + '/template.json'
+    mapping_file = os.path.join(
+        bundle_dir,
+        'mappings',
+        mapping_id + 'Mapping.json'
+    )
+    mapping_json = {
+        'id': mapping_id,
+        'name': mapping_id + 'Mapping',
+        'image': {
+            'type': 'tag',
+            'value': tag_pattern
+        },
+        'policy_ids': [],
+        'registry': registry_pattern,
+        'repository': repo_pattern,
+        'whitelist_ids': [allowlist_id]
+    }
+
+    if debug:
+        print(f'Mapping {allowlist_id} via {mapping_id}')
+
+    # write new mapping file
+    write_json_file(mapping_json, mapping_file)
+
+    # read existing mapping list from bundle template
+    template_json = read_json_file(template_file)
+    mapping_list = template_json['mappings']
+
+    # insert new mapping into specified position of mapping list
+    new_map = {'id': mapping_id}
+    if mapping_position > len(mapping_list):
+        mapping_list.append(new_map)
+        if debug:
+            print(f'Inserted {mapping_id} at end of mapping list')
+    else:
+        mapping_list.insert(mapping_position, new_map)
+    template_json['mapping'] = mapping_list
+
+    # write updated template file
+    write_json_file(template_json, template_file)
